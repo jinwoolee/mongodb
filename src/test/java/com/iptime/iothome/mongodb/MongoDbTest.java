e368f28d4fdd10c0ef98a959ff0f53948d24190b
jw <jwlee1728@gmail.com>
2018-07-25 08:45:41 +0900

    insert, read, update testcase

--- src/test/java/com/iptime/iothome/mongodb/MongoDbTest.java
+++ src/test/java/com/iptime/iothome/mongodb/MongoDbTest.java
@@ -1,28 +1,30 @@
 package com.iptime.iothome.mongodb;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-import java.net.UnknownHostException; 
-import java.util.Set;
+import java.net.UnknownHostException;
 
 import org.bson.Document;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.experimental.categories.Categories.ExcludeCategory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.mongodb.BasicDBObjectBuilder;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
+import com.mongodb.Block;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoWriteConcernException;
 import com.mongodb.MongoWriteException;
-import com.mongodb.WriteResult;
+import com.mongodb.bulk.UpdateRequest;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.UpdateResult;
 
 public class MongoDbTest {
 	
@@ -30,14 +32,35 @@ public class MongoDbTest {
 
 	private MongoClient client;
 	private MongoDatabase db;
+	private Document document; 
+	
+	private final String testCollection = "iot";
+	private final String testId = "testId";
+	private final String testDev = "testDev";
+	private final long testValue = 830325;
 	
 	@Before
 	public void setUp() throws UnknownHostException{
 		client =  new MongoClient();	//default : localhost : 27017
 		db = client.getDatabase("iot");
+		
+		//initData();
 	}
 	
-	@Test
+	public void initData() {
+		document = new Document();
+		document.append("_id", testId);
+		document.append("dev", testDev);
+		document.append("value", testValue);
+		
+		MongoCollection<Document> collection = db.getCollection(testCollection);
+		
+		collection.deleteOne(document);
+		collection.insertOne(document);
+	}
+	
+		
+	//@Test
 	public void test() throws UnknownHostException {
 		/***given***/
 		
@@ -53,7 +76,7 @@ public class MongoDbTest {
 			logger.debug(str);
 	}
 	
-	@Test
+	//@Test
 	public void insertTest() throws UnknownHostException{
 		/***Given***/
 		MongoIterable<String> collections = db.listCollectionNames();
@@ -65,7 +88,9 @@ public class MongoDbTest {
 		/***When***/
 		//emptry object(document) 생성
 		BasicDBObjectBuilder docBuilder = BasicDBObjectBuilder.start();
-		//docBuilder.append("_id", "dev1_" + System.currentTimeMillis());
+		
+		String id = "dev1_" + System.currentTimeMillis();
+		docBuilder.append("_id", id);
 		docBuilder.append("dev", "dev1");
 		docBuilder.append("timestamp", System.currentTimeMillis());
 		docBuilder.append("value", 321);
@@ -80,6 +105,58 @@ public class MongoDbTest {
 		}catch(MongoWriteConcernException e){
 			fail("MongoWriteConcernException");
 		}
+		
+		/***Then***/
+		assertEquals(id, dbObject.get("_id"));
+		logger.debug("{}", "id : " + dbObject.get("_id"));
+	}
+	
+	@Test
+	public void updateTest() {
+		/***given***/
+		MongoCollection<Document> collection = db.getCollection(testCollection);
+		
+		/***when***/
+		//collection.replaceOne(document);
+		UpdateResult updateResult = collection.updateOne(Filters.eq("_id", testId), Updates.combine(Updates.set("value", 830325 + 1000000)));
+		
+		//FindIterable<Document> documents = collection.find(new Document().append("_id", testId));
+		//Document doc = documents.first();
 
+		/***then***/
+		assertEquals(1, updateResult.getModifiedCount());
+		//assertEquals(830325 + 2000000, doc.get("value"));		
+	}
+	
+	@Test
+	public void readTest() {
+		/***given***/
+		
+		Block<Document> printBlock = new Block<Document>() {
+		       //@Override
+		       public void apply(final Document document) {
+		           System.out.println(document.toJson());
+		       }
+		};
+		
+		MongoCollection<Document> collection = db.getCollection(testCollection);
+		
+		/***when***/
+		FindIterable<Document> findIterable = collection.find();
+		findIterable.forEach(printBlock);
+		
+		logger.debug("{}", "===============eq===============");
+		//collection.find(Filters.eq("_id", testId)).forEach(printBlock);
+		findIterable = collection.find(Filters.eq("_id", testId));
+		findIterable = findIterable.projection(new Document().append("dev", null).append("value", null));
+		//findIterable.forEach(printBlock);
+		
+		//여기서부터 
+		Document doc = findIterable.first();
+		logger.debug("{}", "doc.toString() : " + doc.toJson());
+		
+		/***then***/
+		assertEquals(testId, doc.get("_id"));
+		assertEquals(testDev, doc.get("dev"));
 	}
 }
