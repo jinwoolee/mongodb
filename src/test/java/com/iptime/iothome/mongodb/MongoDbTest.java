3fbfb72f6652a1c48bed5ae073720342449745cd
jw <jwlee1728@gmail.com>
2018-07-24 17:27:01 +0900

    junit insert test

--- src/test/java/com/iptime/iothome/mongodb/MongoDbTest.java
+++ src/test/java/com/iptime/iothome/mongodb/MongoDbTest.java
@@ -1,23 +1,85 @@
 package com.iptime.iothome.mongodb;
 
-import java.net.UnknownHostException;
+import static org.junit.Assert.*;
 
+import java.net.UnknownHostException; 
+import java.util.Set;
+
+import org.bson.Document;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Categories.ExcludeCategory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.mongodb.BasicDBObjectBuilder;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoWriteConcernException;
+import com.mongodb.MongoWriteException;
+import com.mongodb.WriteResult;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
 
 public class MongoDbTest {
+	
+	private Logger logger = LoggerFactory.getLogger(MongoDbTest.class);
 
+	private MongoClient client;
+	private MongoDatabase db;
+	
+	@Before
+	public void setUp() throws UnknownHostException{
+		client =  new MongoClient();	//default : localhost : 27017
+		db = client.getDatabase("iot");
+	}
+	
 	@Test
 	public void test() throws UnknownHostException {
 		/***given***/
-		MongoClient client =  new MongoClient();
-		DB db = client.getDB("local");
 		
 		/***when***/
-
+		MongoIterable<String> collections = db.listCollectionNames();
+		logger.debug("{}", collections.toString());
+		
+		MongoCollection<Document> dbc = db.getCollection("iot");
+		logger.debug("{}", dbc.count());
+		
 		/***then***/
+		for(String str : collections)
+			logger.debug(str);
 	}
+	
+	@Test
+	public void insertTest() throws UnknownHostException{
+		/***Given***/
+		MongoIterable<String> collections = db.listCollectionNames();
+		logger.debug("{}", collections.toString());
+		
+		MongoCollection<DBObject> dbc = db.getCollection("iot",DBObject.class);
+		logger.debug("{}", dbc.count());
+		
+		/***When***/
+		//emptry object(document) 생성
+		BasicDBObjectBuilder docBuilder = BasicDBObjectBuilder.start();
+		//docBuilder.append("_id", "dev1_" + System.currentTimeMillis());
+		docBuilder.append("dev", "dev1");
+		docBuilder.append("timestamp", System.currentTimeMillis());
+		docBuilder.append("value", 321);
+		
+		//docBuilder.
+		DBObject dbObject = docBuilder.get();
+		
+		try{
+			dbc.insertOne(dbObject);
+		}catch(MongoWriteException e){
+			fail("MongoWriteException");
+		}catch(MongoWriteConcernException e){
+			fail("MongoWriteConcernException");
+		}
 
+	}
 }
